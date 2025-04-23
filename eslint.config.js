845d1d08886447f65406b4e3da185729dd2bc74b
Federico Pasqua (eisterman) <federico.pasqua.96@gmail.com>
2025-04-23 20:08:42 +0200

    Add eslint, update package version and add .idea project

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import reactHooks from 'eslint-plugin-react-hooks';
+import { defineConfig } from "eslint/config";
+
+// noinspection JSCheckFunctionSignatures
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "varsIgnorePattern": "^_",
+          "argsIgnorePattern": "^_"
+        }
+      ]
+    },
+  },
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
+  reactHooks.configs['recommended-latest'],
+]);
