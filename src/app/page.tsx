bd6bc7709f5de4bc032a8906234e8f9a14034fc2
Federico Pasqua (eisterman) <federico.pasqua.96@gmail.com>
2025-05-04 21:23:23 +0200

    Remove seeding from page, I can launch manually the commands from my computer to prepare the DB!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,8 @@ import Kanban from '@/components/Kanban';
 import { Suspense } from 'react';
 import { HydrateClient, prefetch, trpc } from '@/trpc/server';
 import { db } from '@/db';
-import { testseed } from '../../scripts/testseed';
-import { revalidatePath } from 'next/cache';
-
-// This page uses Postgresql so if it's accidentally rendered statically the BUILD process will
-// try to contact the DB and start to renderize the content based on the DB state at the build moment.
-// Force-dynamic make this page impossible to be rendered statically.
-export const dynamic = 'force-dynamic';
 
 type Items = Record<UniqueIdentifier, UniqueIdentifier[]>;
-
-async function seedAction() {
-  'use server';
-  await testseed();
-  revalidatePath('/', 'page');
-}
 
 export default async function Home() {
   const items: Promise<Items> = new Promise((res) =>
@@ -33,12 +20,7 @@ export default async function Home() {
     ),
   );
   const kanban = await db.query.kanbans.findFirst();
-  if (kanban === undefined)
-    return (
-      <button className='btn' onClick={seedAction}>
-        Reseed DB
-      </button>
-    );
+  if (kanban === undefined) throw new Error('Missing Kanban in DB');
   prefetch(trpc.cards.list.queryOptions({ kanbanId: kanban.id }));
 
   return (
@@ -46,11 +28,8 @@ export default async function Home() {
       <main className='flex items-start justify-center pt-4 pb-4 h-full'>
         <div className='flex-1 flex flex-col items-center gap-4 h-full'>
           <header className='flex flex-col items-center gap-9'>
-            <div className='w-[500px] max-w-[100vw] p-4 text-center flex justify-center items-center gap-8'>
+            <div className='w-full p-4 text-center flex justify-center items-center'>
               <h2 className='text-6xl'>Grello</h2>
-              <button className='btn' onClick={seedAction}>
-                Reseed DB
-              </button>
             </div>
           </header>
           <div className='flex flex-row justify-start items-stretch gap-8 w-screen h-full overflow-x-auto px-4 md:px-8'>
