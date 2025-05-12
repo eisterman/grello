8722c2759da8a61fb458806ac17c246c5c4f3ae8
Federico Pasqua (eisterman) <federico.pasqua.96@gmail.com>
2025-05-12 17:39:37 +0200

    In tRPC Card router replace `opts` with `{ ctx, input }` for more legibility

--- src/trpc/routers/cards.ts
+++ src/trpc/routers/cards.ts
@@ -6,18 +6,20 @@ import { TRPCError } from '@trpc/server';
 import { eq } from 'drizzle-orm';
 
 export const cardsRouter = createTRPCRouter({
-  list: baseProcedure.query(async (opts) => {
-    return await opts.ctx.db.query.cards.findMany({
+  list: baseProcedure.query(async ({ ctx }) => {
+    return await ctx.db.query.cards.findMany({
       orderBy: (cards, { asc }) => asc(cards.id),
     });
   }),
-  retrieve: baseProcedure.input(z.object({ id: z.number().gt(0) })).query(async (opts) => {
-    const card = await opts.ctx.db.query.cards.findFirst({ where: eq(cards.id, opts.input.id) });
-    if (card === undefined) throw new TRPCError({ code: 'NOT_FOUND' });
-    return card;
-  }),
-  create: baseProcedure.input(createInsertSchema(cards)).mutation(async (opts) => {
-    return await opts.ctx.db.insert(cards).values(opts.input).returning();
+  retrieve: baseProcedure
+    .input(z.object({ id: z.number().gt(0) }))
+    .query(async ({ ctx, input }) => {
+      const card = await ctx.db.query.cards.findFirst({ where: eq(cards.id, input.id) });
+      if (card === undefined) throw new TRPCError({ code: 'NOT_FOUND' });
+      return card;
+    }),
+  create: baseProcedure.input(createInsertSchema(cards)).mutation(async ({ ctx, input }) => {
+    return await ctx.db.insert(cards).values(input).returning();
   }),
   update: baseProcedure
     .input(
@@ -26,11 +28,8 @@ export const cardsRouter = createTRPCRouter({
         payload: createUpdateSchema(cards),
       }),
     )
-    .mutation(async (opts) => {
-      const qres = await opts.ctx.db
-        .update(cards)
-        .set(opts.input.payload)
-        .where(eq(cards.id, opts.input.id));
+    .mutation(async ({ ctx, input }) => {
+      const qres = await ctx.db.update(cards).set(input.payload).where(eq(cards.id, input.id));
       if (qres.rowCount === null) throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR' });
       if (qres.rowCount === 0)
         throw new TRPCError({
@@ -39,14 +38,16 @@ export const cardsRouter = createTRPCRouter({
         });
       return qres.rowCount;
     }),
-  destroy: baseProcedure.input(z.object({ id: z.number().gt(0) })).mutation(async (opts) => {
-    const qres = await opts.ctx.db.delete(cards).where(eq(cards.id, opts.input.id));
-    if (qres.rowCount === null) throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR' });
-    if (qres.rowCount === 0)
-      throw new TRPCError({
-        code: 'NOT_FOUND',
-        message: "Cannot delete object that doesn't exists",
-      });
-    return qres.rowCount;
-  }),
+  destroy: baseProcedure
+    .input(z.object({ id: z.number().gt(0) }))
+    .mutation(async ({ ctx, input }) => {
+      const qres = await ctx.db.delete(cards).where(eq(cards.id, input.id));
+      if (qres.rowCount === null) throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR' });
+      if (qres.rowCount === 0)
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: "Cannot delete object that doesn't exists",
+        });
+      return qres.rowCount;
+    }),
 });
